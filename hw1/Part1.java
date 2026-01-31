379a30bdeb1ca53de3e591d1e296854032adba18
elizabethdinella <elizabeth.a.dinella@gmail.com>
2026-01-30 20:21:18 -0500

    template fixes

--- hw1/Part1.java
+++ hw1/Part1.java
@@ -13,14 +13,14 @@ public class Part1 {
      * @param message the message to hash
      * @param hashFunction which hash to use:
      *                     1 = full SHA-256 (256 bits)
-     *                     2 = truncated to 8 bits
-     *                     3 = truncated to 16 bits
+     *                     2 = SHA-256 truncated to 8 bits
+     *                     3 = SHA-256 truncated to 16 bits
      *                     other = print error and return null
      * @return the message digest as a byte array
      */
-    public static byte[] computeDigest(byte[] message, int hashFunction) 
-            throws NoSuchAlgorithmException {
-        // TODO
+    public static byte[] computeDigest(byte[] message, int hashFunction)  throws NoSuchAlgorithmException {
+        
+        // TODO  
         throw new UnsupportedOperationException("TODO");
     }
     
@@ -33,20 +33,10 @@ public class Part1 {
      * @return true if message's digest matches expectedDigest
      */
     public static boolean verifyIntegrity(byte[] message, byte[] expectedDigest, 
-                                          int hashFunction) 
-            throws NoSuchAlgorithmException {
+                                          int hashFunction) throws NoSuchAlgorithmException {
+
         // TODO
         throw new UnsupportedOperationException("TODO");
     }
     
-    /**
-     * Attempt to find two different messages with the same digest.
-     * 
-     * @param hashFunction which hash to test (1, 2, or 3)
-     * @return two different messages with same digest, or null if not found
-     */
-    public static String[] findCollision(int hashFunction) throws Exception {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
-    }
 }
